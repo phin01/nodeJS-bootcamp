9ee10cf5b8e16c9d61072c4ddb66377503203c2d
phin01 <phin01@gmail.com>
2023-03-18 17:37:29 -0300

    create winston logger

--- my-bank-api/index.js
+++ my-bank-api/index.js
@@ -1,8 +1,30 @@
 import express from "express";
+import winston from "winston";
 import accountRouter from './routes/accounts.js';
 import { promises as fs } from "fs";
 
 global.fileName = 'accounts.json';
+
+const {combine, timestamp, label, printf} = winston.format;
+
+const logFormat = printf(
+    ({ level, message, label, timestamp }) => {
+        return `${timestamp} [${label}] ${level}: ${message} `;
+    }
+);
+
+global.logger = winston.createLogger({
+    level: 'silly',
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({ filename: 'my-bank-api.log' }),
+    ],
+    format: combine(
+        label({ label: 'my-bank-api', }),
+        timestamp(),
+        logFormat
+    )
+});
 
 const app = new express();
 app.use(express.json());
@@ -15,14 +37,14 @@ app.listen(3000, async () => {
     // if it doesn't, create it with initial config    
     try {
         await fs.readFile(global.fileName);
-        console.log('API started with existing data');
+        global.logger.info('API started with existing data');
     } catch (error) {
         const initialJson = {
             nextId: 1,
             accounts: []
         }
         fs.writeFile(global.fileName, JSON.stringify(initialJson))
-            .then(() => console.log('API started with initial blank data'))
-            .catch(err => console.log(err));
+            .then(() => global.logger.info('API started with initial blank data'))
+            .catch(err => global.logger.error(err));
     }
 });
