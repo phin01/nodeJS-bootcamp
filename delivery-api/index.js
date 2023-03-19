3c1173b5fab1297b2d60e6ae26b92b0cc06f7bf9
phin01 <phin01@gmail.com>
2023-03-19 16:32:10 -0300

    create initial index.js

---
+++ delivery-api/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import winston from "winston";
+
+global.fileName = 'pedidos.json';
+
+const {combine, timestamp, label, printf} = winston.format;
+
+const logFormat = printf(
+    ({ level, message, label, timestamp }) => {
+        return `${timestamp} [${label}] ${level}: ${message} `;
+    }
+);
+
+global.logger = winston.createLogger({
+    level: 'info',
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({ filename: 'deliveries-api.log' }),
+    ],
+    format: combine(
+        label({ label: 'deliveries-api', }),
+        timestamp(),
+        logFormat
+    )
+});
+
+const app = new express();
+app.use(express.json());
+
+app.listen(3000, async () => {
+
+    // check if orders.json file exists
+    try {
+        await fs.readFile(global.fileName);
+        global.logger.info('API started with existing data');
+    } catch (error) {
+        global.logger.error('Could not load order data');
+    }
+});
