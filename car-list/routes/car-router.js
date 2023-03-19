cffa1e4b1d8a2e9fcb1fa5c31da74b68fca46ff4
phin01 <phin01@gmail.com>
2023-03-18 21:11:58 -0300

    include route for least models

--- car-list/routes/car-router.js
+++ car-list/routes/car-router.js
@@ -35,6 +35,47 @@ router.get('/mostModels', async (req, res, next) => {
             );
 
         res.send(max);
+        console.log(carList);
+        
+    } catch (error) {
+        next(error);
+    }
+
+});
+
+
+// Return brand with most models
+// In case it's a tie, return a list with all brands
+router.get('/leastModels', async (req, res, next) => {
+
+    try {
+        let carList = JSON.parse(await fs.readFile(filename));
+        
+        // create property with array size
+        carList.forEach(element => {
+            element.count = element.models.length
+        });
+
+        // sort by array size, descending
+        carList = carList.sort((a, b) => {
+            if (a.count < b.count) { return -1; }
+        });
+
+        // define max array size
+        const minModels = carList[0].count;
+
+        // filter original list by max size
+        const minBrands = carList.filter(
+            (brand) => brand.count === minModels
+        );
+
+        const min = [];
+        minBrands.forEach(element => {
+            min.push(element.brand)}
+            );
+
+        res.send(min);
+        console.log(carList);
         
     } catch (error) {
         next(error);
